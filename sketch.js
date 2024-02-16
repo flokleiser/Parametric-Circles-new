0bdbf9720518adea01de30a33151fddb69be4d88
flononymus <flononymus@gmail.com>
2024-02-16 11:11:05 +0100

    better way of controlling the mouse now

--- sketch.js
+++ sketch.js
@@ -67,6 +67,8 @@ function setup() {
   background(50);
   // background(230);
 
+  followMouse = true;
+
   centerX = width / 2;
   centerY = height / 2;
 
@@ -76,6 +78,14 @@ function setup() {
   resetButton = createButton('RESET');
   resetButton.position(10,10);
   resetButton.mousePressed(resetCanvas);
+
+  controlButton= createButton('Follow Mouse');
+  controlButton.position(240,10);
+  // controlButton.mousePressed(followMouse);
+  controlButton.mousePressed(function() {
+    followMouse = !followMouse
+  });
+
 
   sel = createSelect();
   sel.position(80,10);
@@ -736,6 +746,13 @@ function randomSwitch() {
       newLine5 = !newLine5;
     }
   lastSwitchTime = millis();
+  }
+}
+
+function mouseClicked() {
+  if (followMouse) {
+  drawing = !drawing;
+  console.log('Drawing: ', drawing);
   }
 }
 
